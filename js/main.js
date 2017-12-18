29954708b2f7fefe78b07ebe3df27a40f2d6a45a
AnaisOlivares <anaisolivareshilario@gmail.com>
2017-12-18 00:51:38 -0500

    añadiendoo menu

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//Probando jQuery!
+
+  // Show sideNav
+  $('.button-collapse').sideNav('show');
+  // Hide sideNav
+  $('.button-collapse').sideNav('hide');
+  // Destroy sideNav
+  $('.button-collapse').sideNav('destroy');
